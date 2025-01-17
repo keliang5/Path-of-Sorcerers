e5487c0055b628e2dd1c21dec2a03340351a2510
keliang5 <159198151+keliang5@users.noreply.github.com>
2025-01-17 11:21:59 -0800

    L.6 The weapon + L.7 Player's Hands

---
+++ M10.twin_stick_workbook/weaponpivot.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction := Vector2.ZERO
+	direction =  global_position.direction_to(get_global_mouse_position())
+	
+	rotation = direction.angle()
